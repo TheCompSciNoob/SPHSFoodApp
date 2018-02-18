2762285f7cefad79df13229b2e805f69897f0178
TheCompSciNoob <kyroschow@gmail.com>
2018-02-17 23:55:49 -0800

    what did I just do?

---
+++ app/src/main/java/com/example/kyros/sphsfoodapp/MealAPI.kt
@@ -0,0 +1,26 @@
+package com.example.kyros.sphsfoodapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by Kyros on 2/17/2018.
+ */
+interface MealAPI {
+    companion object {
+        val baseURL = "http://www/themealdb.com/api/json/v1/1"
+    }
+
+    @GET("random.php")
+    fun getRandomOne() : Call<Meal>
+
+    @GET("filter.php")
+    fun filterByCategory(@Query("c") category : String)
+
+    @GET("filter.php")
+    fun filterByArea(@Query("a") area : String)
+
+    @GET("filter.php")
+    fun filterByIngredient(@Query("i") ingredient : String)
+}
